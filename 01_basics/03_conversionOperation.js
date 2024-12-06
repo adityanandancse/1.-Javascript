a17fdf288a03bb5d706cb517cceb9b1dfbb90024
Aditya Nandan <cse.adityanandan@gmail.com>
2024-12-06 22:21:17 +0000

    Extended 03_conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,42 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
     // String
+
+    // ----- Operations -----
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+    //-3
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+// ----- Addition of strings -----
+
+let str1 = "hello";
+let str2 = " Aditya";
+
+let str3 = str1 + str2;
+console.log("str3");
+    //hello Aditya
+
+console.log("1" + 2);
+    // 12
+console.log(2 + "1");
+    // 12
+console.log("1" + 2 + 2);
+    //122
+console.log(1 + 2 + "3")
+    //33
+
+// If the first operation is string; the result will be converted to string
+// If the first and second operation is a number and last one is string; first and second will perform the operation and the last string will be added to the number
+
+ 
+
+
