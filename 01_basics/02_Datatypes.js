c36e43d555b6d0ecf3f5261abdf9d34831fa1e7a
Aditya Nandan <cse.adityanandan@gmail.com>
2024-12-06 21:39:56 +0000

    Added datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; 
+    //Treat all JS code as newer version
+// alert("3 + 3") 
+    //We are using node.js, not browser
+    // Don't use it for arithematics
+
+console.log(3 + 3)
+console.log("Aditya")
+
+let name = "Aditya"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null
+    // It is a standalone value 
+    // Empty tag
+// undefined 
+    // When value isn't defined/assigned
+// symbol => unique
+
+// object 
+console.log(typeof "Aditya"); 
+    // String
+console.log(typeof age);
+    // Number
+console.log(typeof null);
+    //object
+console.log(typeof undefined);
+    //undefined
