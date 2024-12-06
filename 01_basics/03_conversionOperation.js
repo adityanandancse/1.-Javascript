109df463233f8f5f6ed748530d186461b75b618e
Aditya Nandan <cse.adityanandan@gmail.com>
2024-12-06 21:58:30 +0000

    Added conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = 33
+// console.log(typeof score);
+     // string
+// console.log(typeof (score));
+     // string
+
+    let score = "33";
+    console.log(typeof score);
+        // string
+    console.log(typeof (score));
+        // string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+    // number
+console.log(valueInNumber);
+    // NaN
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+    //True
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => False
+// "Aditya" => True
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+    // String
